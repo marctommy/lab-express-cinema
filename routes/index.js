82554bd5e513a68bcf65ae09deac80191a00b619
Marc Wiesener <wiesener.marc@gmail.com>
2022-02-16 21:51:18 +0100

    with basic css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const Movie = require("../models/Movie.model"); // file path
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  // url in the browser "/"
+  Movie.find()
+    .then((allMovies) => {
+      //   res.send(allBooks);
+      res.render("movies/movie", { movies: allMovies }); // rendering the hbs "no /"
+    })
+    .catch((error) => console.log(error));
+});
+
+router.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then((result) => {
+      console.log(result);
+      res.render("movies/movie-details", { movie: result });
+    })
+    .catch((error) => console.log(error));
+});
 
 module.exports = router;
